c98eec3997cd922186009585fa01a0f748ecd8ea
vipul <vipulofficial206@gmail.com>
2025-04-13 10:47:19 +0530

    Resolve main.js conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -5,9 +6,11 @@ import { HashRouter, HashRouter as Router } from 'react-router-dom';
 import Storecontextprovider from './context/Storecontext.jsx'
 
 createRoot(document.getElementById('root')).render(
+  <StrictMode>
  <HashRouter>
     <Storecontextprovider>
       <App/>
     </Storecontextprovider>
     </HashRouter>
+    </StrictMode>
 )
